e08994dc0df84675bed645365324b4b4c629625c
Brandon <brandonbsuen@gmail.com>
2020-03-04 20:24:18 -0800

    Fixed class props

--- src/components/LinkedIn.js
+++ src/components/LinkedIn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import makeAsyncScriptLoader from 'react-async-script';
 
 const Badge = () => (
-	<div class='LI-profile-badge' data-version='v1' data-size='medium' data-locale='en_US' data-type='vertical' data-theme='light' data-vanity='brandon-suen-8ba1b3187'><a class='LI-simple-link' href='https://www.linkedin.com/in/brandon-suen-8ba1b3187?trk=profile-badge'>Brandon Suen</a></div>
+	<div className='LI-profile-badge' data-version='v1' data-size='medium' data-locale='en_US' data-type='vertical' data-theme='light' data-vanity='brandon-suen-8ba1b3187'><a className='LI-simple-link' href='https://www.linkedin.com/in/brandon-suen-8ba1b3187?trk=profile-badge'>Brandon Suen</a></div>
 );
 
 const LinkedIn = makeAsyncScriptLoader('https://platform.linkedin.com/badges/js/profile.js', { removeOnUnmount: true })(Badge);
