39718b709a5ec28ff9b14aebcb75db9566a7a267
Brandon <brandonbsuen@gmail.com>
2020-03-09 16:39:45 -0700

    Resume added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,26 @@ import Calculator from '../components/Calculator';
 import Layout from '../components/Layout';
 import LinkedIn from '../components/LinkedIn';
 import QuizWatch from '../components/QuizWatch';
+import resume from '../images/resume.png';
 import styles from '../styles/index.module.scss';
 
+const resumeHref = '/brandon-suen-resume.pdf';
+
+const ResumeLink = props => (
+	<a download href={resumeHref} target='_blank'>
+		{props.children}
+	</a>
+);
+
 export default class Home extends React.Component {
+	state = {
+		opacity: 0
+	};
+
+	_activate = () => this.setState({ opacity: 0.2 });
+
+	_deactivate = () => this.setState({ opacity: 0 });
+
 	render() {
 		return (
 			<Layout title='Home'>
@@ -44,18 +61,46 @@ export default class Home extends React.Component {
 				<div className={styles.content}>
 					<div className={styles.bio}>
 						<div className={styles.bioText}>
-							<h3>
-								Who I am
-							</h3>
+							<h3>Who I am</h3>
 							<p>
-								I'm a
-								programmer forever driven by a passion to build and learn new
-								things. To build my websites, I use React.js on the front-end. I use Sass
-								for styling and Node.js on the back-end. I make my mobile apps
-								using the React Native framework. If you'd like me to build you an app or website or you'd like to get in touch for another reason, feel free to connect with me on LinkedIn or <a href='#contact'>contact me below</a>.
+								I'm a programmer forever driven by a passion to build and learn
+								new things. To build my websites, I use React.js on the
+								front-end. I use Sass for styling and Node.js on the back-end. I
+								make my mobile apps using the React Native framework. If you'd
+								like me to build you an app or website or you'd like to get in
+								touch for another reason, feel free to connect with me on
+								LinkedIn or <a href='#contact'>contact me below</a>.
 							</p>
 						</div>
 						<LinkedIn />
+					</div>
+					<div className={styles.white}>
+						<h2>Résumé</h2>
+						<div className={styles.resumeContainer}>
+							<div>
+								<ResumeLink>
+									<img
+										alt="Brandon Suen's resume"
+										className={styles.resume}
+										src={resume}
+									/>
+									<div
+										className={styles.overlay}
+										onMouseOver={this._activate}
+										onMouseLeave={this._deactivate}
+										style={{ opacity: this.state.opacity }}
+									/>
+								</ResumeLink>
+							</div>
+							<p>
+								<ResumeLink>Download*</ResumeLink>
+								{'   |   '}
+								<a href={resumeHref} target='_blank'>
+									Open in new tab
+								</a>
+							</p>
+							<p className={styles.small}>*Not supported in all browsers</p>
+						</div>
 					</div>
 					<div className={styles.white}>
 						<h2>Featured Work</h2>
@@ -66,7 +111,12 @@ export default class Home extends React.Component {
 						</Link>
 					</div>
 				</div>
-				<script type='text/javascript' src='https://platform.linkedin.com/badges/js/profile.js' async defer></script>
+				<script
+					type='text/javascript'
+					src='https://platform.linkedin.com/badges/js/profile.js'
+					async
+					defer
+				></script>
 			</Layout>
 		);
 	}
