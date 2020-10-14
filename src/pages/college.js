181517c4ede1325d6e0778aa9ba839547686b31a
Brandon Suen <brandonsuendeveloper@gmail.com>
2020-10-13 17:17:24 -0700

    Added target blank to outbound links

--- src/pages/college.js
+++ src/pages/college.js
@@ -3,6 +3,14 @@ import React from 'react';
 import Layout from '../components/Layout';
 import styles from '../styles/college.module.scss';
 
+const CourseLink = ({ children, href }) => (
+	<p>
+		<a href={href} target='_blank'>
+			{children}
+		</a>
+	</p>
+);
+
 const College = () => (
 	<Layout title='College Coursework'>
 		<div className={styles.title}>
@@ -13,41 +21,29 @@ const College = () => (
 			<h3>Fall 2020</h3>
 			<div className={styles.indent}>
 				<ul>
-					<p>
-						<a href='https://classes.berkeley.edu/content/2020-fall-classic-r44-001-lec-001'>
-							CLASSIC R44: Roots of Western Civilization
-						</a>
-					</p>
-					<p>
-						<a href='https://classes.berkeley.edu/content/2020-fall-compsci-61b-001-lec-001'>
-							COMPSCI 61B: Data Structures
-						</a>
-					</p>
-					<p>
-						<a href='https://classes.berkeley.edu/content/2020-fall-eecs-16a-001-lec-001'>
-							EECS 16A: Designing Information Devices and Systems I
-						</a>
-					</p>
-					<p>
-						<a href='https://classes.berkeley.edu/content/2020-fall-nusctx-10-001-lec-001'>
-							NUSCTX 10: Introduction to Human Nutrition
-						</a>
-					</p>
-					<p>
-						<a href='https://classes.berkeley.edu/content/2020-fall-ugis-192e-005-tut-005'>
-							UGIS 192E: Supervised Research: Interdisciplinary Studies
-						</a>
-					</p>
+					<CourseLink href='https://classes.berkeley.edu/content/2020-fall-classic-r44-001-lec-001'>
+						CLASSIC R44: Roots of Western Civilization
+					</CourseLink>
+					<CourseLink href='https://classes.berkeley.edu/content/2020-fall-compsci-61b-001-lec-001'>
+						COMPSCI 61B: Data Structures
+					</CourseLink>
+					<CourseLink href='https://classes.berkeley.edu/content/2020-fall-eecs-16a-001-lec-001'>
+						EECS 16A: Designing Information Devices and Systems I
+					</CourseLink>
+					<CourseLink href='https://classes.berkeley.edu/content/2020-fall-nusctx-10-001-lec-001'>
+						NUSCTX 10: Introduction to Human Nutrition
+					</CourseLink>
+					<CourseLink href='https://classes.berkeley.edu/content/2020-fall-ugis-192e-005-tut-005'>
+						UGIS 192E: Supervised Research: Interdisciplinary Studies
+					</CourseLink>
 				</ul>
 			</div>
 			<h3>Summer 2020</h3>
 			<div className={styles.indent}>
 				<ul>
-					<p>
-						<a href='https://classes.berkeley.edu/content/2020-summer-compsci-61a-001-lec-001'>
-							COMPSCI 61A: The Structure and Interpretation of Computer Programs
-						</a>
-					</p>
+					<CourseLink href='https://classes.berkeley.edu/content/2020-summer-compsci-61a-001-lec-001'>
+						COMPSCI 61A: The Structure and Interpretation of Computer Programs
+					</CourseLink>
 				</ul>
 			</div>
 		</div>
--- src/pages/online-courses.js
+++ src/pages/online-courses.js
@@ -17,6 +17,14 @@ const {
 
 const DImage = ({ img }) => <DynamicImage className={styles.img} img={img} />;
 
+const Header = ({ children, href }) => (
+	<h4>
+		<a href={href} target='_blank'>
+			{children}
+		</a>
+	</h4>
+);
+
 const OnlineCourses = () => (
 	<Layout title='Online Courses'>
 		<div className={styles.title}>
@@ -27,54 +35,40 @@ const OnlineCourses = () => (
 			<Link to={'/projects?program=' + programs.freeCodeCamp.name}>
 				See projects I've made for the {programs.freeCodeCamp.name} curriculum
 			</Link>
-			<h4>
-				<a href='https://www.freecodecamp.org/certification/brandons42/responsive-web-design'>
-					Responsive Web Design Certification
-				</a>
-			</h4>
+			<Header href='https://www.freecodecamp.org/certification/brandons42/responsive-web-design'>
+				Responsive Web Design Certification
+			</Header>
 			<DImage img={fccRWDCert} />
-			<h4>
-				<a href='https://www.freecodecamp.org/certification/brandons42/javascript-algorithms-and-data-structures'>
-					JavaScript Algorithms and Data Structures Certification
-				</a>
-			</h4>
+			<Header href='https://www.freecodecamp.org/certification/brandons42/javascript-algorithms-and-data-structures'>
+				JavaScript Algorithms and Data Structures Certification
+			</Header>
 			<DImage img={fccJSCert} />
 			<h3>{programs.drlnd.name}</h3>
 			<Link to={'/projects?program=' + programs.drlnd.name}>
 				See projects I've made for the {programs.drlnd.name} curriculum
 			</Link>
-			<h4>
-				<a href='https://confirm.udacity.com/GNEDA6YE'>
-					Deep Reinforcement Learning Nanodegree
-				</a>
-			</h4>
+			<Header href='https://confirm.udacity.com/GNEDA6YE'>
+				Deep Reinforcement Learning Nanodegree
+			</Header>
 			<DImage img={udacityDRLNDCert} />
 			<h3>Forage</h3>
-			<h4>
-				<a href='https://insidesherpa.s3.amazonaws.com/completion-certificates/JP%20Morgan/R5iK7HMxJGBgaSbvk_JPMorgan%20Chase_MEPQbTyfJ63yY6n4b_completion_certificate.pdf'>
-					JPMorgan Chase Software Engineering Virtual Internship
-				</a>
-			</h4>
+			<Header href='https://insidesherpa.s3.amazonaws.com/completion-certificates/JP%20Morgan/R5iK7HMxJGBgaSbvk_JPMorgan%20Chase_MEPQbTyfJ63yY6n4b_completion_certificate.pdf'>
+				JPMorgan Chase Software Engineering Virtual Internship
+			</Header>
 			<DImage img={jpMorganChaseCert} />
 			<h3>edX</h3>
-			<h4>
-				<a href='https://courses.edx.org/certificates/7f28da8378f24d35b898e5561af432eb'>
-					Agile Development Using Ruby on Rails - The Basics
-				</a>
-			</h4>
+			<Header href='https://courses.edx.org/certificates/7f28da8378f24d35b898e5561af432eb'>
+				Agile Development Using Ruby on Rails - The Basics
+			</Header>
 			<DImage img={edXAgileCert} />
 			<h3>Kaggle</h3>
-			<h4>
-				<a href='https://www.kaggle.com/learn/certification/brandons42/python'>
-					Python
-				</a>
-			</h4>
+			<Header href='https://www.kaggle.com/learn/certification/brandons42/python'>
+				Python
+			</Header>
 			<DImage img={kagglePythonCert} />
-			<h4>
-				<a href='https://www.kaggle.com/learn/certification/brandons42/intro-to-machine-learning'>
-					Intro to Machine Learning
-				</a>
-			</h4>
+			<Header href='https://www.kaggle.com/learn/certification/brandons42/intro-to-machine-learning'>
+				Intro to Machine Learning
+			</Header>
 			<DImage img={kaggleMLCert} />
 		</div>
 	</Layout>
