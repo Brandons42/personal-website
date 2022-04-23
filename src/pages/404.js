14bfc3a87e1243689603b2d14fe1a54fd54c9dfd
Brandon Suen <brandonsuendeveloper@gmail.com>
2022-04-23 00:58:55 -0700

    404 message updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,9 @@ import styles from '../styles/404.module.scss';
 const NotFoundPage = () => (
 	<Layout title='404: Not found'>
 		<div>
-			<h2 className={styles.header}>NOT FOUND</h2>
+			<h2 className={styles.header}>Not Found</h2>
 			<p className={styles.body}>
-				You just hit a route that doesn&#39;t exist... the sadness.
+				Sorry, but the page you're looking for doesn't seem to be here.
 			</p>
 		</div>
 	</Layout>
